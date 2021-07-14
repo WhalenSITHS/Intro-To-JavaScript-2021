f7a20a355644da6be102b0a62a1272607709f86a
mr. whalen <michael.whalen@sitechhs.com>
2021-07-14 13:26:03 -0400

    adsfg

--- index.js
+++ index.js
@@ -1,26 +1,9 @@
-let variable; //variable declaration
-variable = "Variable"; //variable defintion
-let number = 7; //number data type
-let string = "Hello"; //string data type
-let truthy = true; //boolean
-let falsey = false; //booleans
-let bigString = "hello I'm a bigger string"; //camel case
+//Strings
+const str = "Hello, my name is";
+const name = "Mike";
+const greet = "How are you today?";
+const newStr = str + name + greet; //concatenation
 
-var variable2 = "var"; //we dont do this
-let variable3; // we can define later OR REDEFINE
-variable = "nope";
-//console.log(variable);
-const constant = "constant"; //cannot redefine, use this 95% of the time
-//constant = "liosdjafhgbnilk";
-
-/* const nullTest = null; //absence of value
-const undefinedTest = "test";
-
-console.log(nullTest); */
-const number1 = 19824;
-const number2 = 72;
-let add = number1 + number2;
-let sub = number1 - number2;
-let mult = number1 * number2;
-let div = number1 / number2;
-console.log(mult, div);
+//template literals
+const literal = `This is a new string, is it not cool? ${str} ${name} ${greet}`;
+console.log(literal);
