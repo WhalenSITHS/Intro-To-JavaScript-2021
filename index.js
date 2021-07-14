e0ccb4b840c1021c562fb0b8287a791e67409b5e
mr. whalen <michael.whalen@sitechhs.com>
2021-07-14 13:20:08 -0400

    sohe

--- index.js
+++ index.js
@@ -9,6 +9,18 @@ let bigString = "hello I'm a bigger string"; //camel case
 var variable2 = "var"; //we dont do this
 let variable3; // we can define later OR REDEFINE
 variable = "nope";
-console.log(variable);
+//console.log(variable);
 const constant = "constant"; //cannot redefine, use this 95% of the time
 //constant = "liosdjafhgbnilk";
+
+/* const nullTest = null; //absence of value
+const undefinedTest = "test";
+
+console.log(nullTest); */
+const number1 = 19824;
+const number2 = 72;
+let add = number1 + number2;
+let sub = number1 - number2;
+let mult = number1 * number2;
+let div = number1 / number2;
+console.log(mult, div);
