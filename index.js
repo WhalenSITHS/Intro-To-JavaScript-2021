42fb7939960ac0bf6940ee49e528054418f3d9be
mr. whalen <michael.whalen@sitechhs.com>
2021-07-14 08:10:42 -0400

    asdf

---
+++ index.js
@@ -0,0 +1,14 @@
+let variable; //variable declaration
+variable = "Variable"; //variable defintion
+let number = 7; //number data type
+let string = "Hello"; //string data type
+let truthy = true; //boolean
+let falsey = false; //booleans
+let bigString = "hello I'm a bigger string"; //camel case
+
+var variable2 = "var"; //we dont do this
+let variable3; // we can define later OR REDEFINE
+variable = "nope";
+console.log(variable);
+const constant = "constant"; //cannot redefine, use this 95% of the time
+//constant = "liosdjafhgbnilk";
