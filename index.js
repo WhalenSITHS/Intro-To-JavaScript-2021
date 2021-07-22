fda3353b7e45586c193e8f2122f78400846a0806
mr. whalen <michael.whalen@sitechhs.com>
2021-07-22 13:27:01 -0400

    sdfgsdfg

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-let variable; //variable declaration
-variable = "Variable"; //variable defintion
-let number = 7; //number data type
-let string = "Hello"; //string data type
-let truthy = true; //boolean
-let falsey = false; //booleans
-let bigString = "hello I'm a bigger string"; //camel case
-
-var variable2 = "var"; //we dont do this
-let variable3; // we can define later OR REDEFINE
-variable = "nope";
-console.log(variable);
-const constant = "constant"; //cannot redefine, use this 95% of the time
-//constant = "liosdjafhgbnilk";
+const DOMSelectors = {
+  button: document.getElementById("btn"),
+  text: document.querySelector("#text"),
+  point: document.querySelectorAll(".point"),
+  pointTwo: document.getElementById("pointTwo"),
+};
+console.log(DOMSelectors.pointTwo);
