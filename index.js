4585ba097a53b89933370fe5582c4cb6ab01caf3
mr. whalen <michael.whalen@sitechhs.com>
2021-07-22 08:10:35 -0400

    sdfgsdfgsgdf

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-///Arrays it is a list
-/* const arr = [3, 6, 9];
+const numbersArr = [1, 2, 3, 4, 5];
 
-console.log(arr[0]); //0 index
-console.log(arr[1]); //0 index
-console.log(arr[2]); //0 index
-console.log(arr[3]); //0 index */
+/* numbersArr.forEach((number) => {
+  //iterate through the array and run function on each element
+  //pass each element into the function as a parameter
+  console.log(number * 2);
+  console.log(numbersArr);
+}); */
 
-const arr2 = [true, false, "yay!", [3, 6, 7], null, 9];
-//console.log(arr2.length);
+const newArr = numbersArr.map((number) => {
+  return number * 2;
+});
+const newArr = numbersArr.map((number) => number * 2);
 
-arr2.unshift("pushed");
-arr2.pop();
-console.log(arr2);
+console.log(newArr);
