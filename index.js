31fa145f1e47c40a57a7c18df5fcf66ed7601bd8
mr. whalen <michael.whalen@sitechhs.com>
2021-07-27 13:16:52 -0400

    sdfgsdfg

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 const DOMSelectors = {
   button: document.getElementById("btn"),
-  text: document.querySelector("#text"),
-  box: document.getElementById("big-black-box"),
-  points: document.querySelectorAll(".point"), //node list
+  box: document.getElementById("container-box"),
+  input: document.querySelector(`#input`),
 };
-
-function backgroundAndText(background, text) {
-  background.style.backgroundColor = "red";
-  text.textContent = "This is now a big bigger red box";
-  text.style.fontSize = "40px";
-}
-
-backgroundAndText(DOMSelectors.box, DOMSelectors.text);
-
-function changeLi() {
-  DOMSelectors.points.forEach((point) => {});
-}
+/* const cat = "meow";
+DOMSelectors.box.insertAdjacentHTML("afterend", `<h1>We are an ${cat}</h1>`); */
+DOMSelectors.button.addEventListener("click", function () {
+  let input = DOMSelectors.input.value;
+  DOMSelectors.box.insertAdjacentHTML("beforeend", `<p> ${input}</p>`);
+  DOMSelectors.input.value = "";
+});
