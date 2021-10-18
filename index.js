f7faac44227df515bebca8939ee1d0a7a5aa7c11
mr. whalen <michael.whalen@sitechhs.com>
2021-10-18 11:39:47 -0400

    sdfsdf

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-let variable; //variable declaration
-variable = "Variable"; //variable defintion
-let number = 7; //number data type
-let string = "Hello"; //string data type
-let truthy = true; //boolean
-let falsey = false; //booleans
-let bigString = "hello I'm a bigger string"; //camel case
+function greet(name) {
+  const greetPromise = new Promise(function (resolve, reject) {
+    resolve(`Hello ${name}`);
+  });
+  return greetPromise;
+}
 
-var variable2 = "var"; //we dont do this
-let variable3; // we can define later OR REDEFINE
-variable = "nope";
-console.log(variable);
-const constant = "constant"; //cannot redefine, use this 95% of the time
-//constant = "liosdjafhgbnilk";
+const suzie = greet("suzie");
+
+suzie.then((hello) => {
+  console.log(hello);
+});
