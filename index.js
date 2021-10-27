894e5d192524a96fd21d26ed5a9ffb272c54469a
mr. whalen <michael.whalen@sitechhs.com>
2021-10-27 09:23:28 -0400

    messing around

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //functions
 
-function test() {
+/* function test() {
   console.log("THis is a test");
 } //declare the function,
 test(); //call the function
@@ -19,3 +19,31 @@ function add(x, y) {
 //console.log(add(7, 9));
 const answer = add(7, 9);
 console.log(answer);
+ */
+
+//console.log(returnMe());
+
+/* function sentence(x) {
+  returnMe(x);
+  console.log(`This is a sentence using ${x}`);
+}
+sentence(90);
+ */
+/* let x = 2;
+function addOne() {
+  return x + 1;
+}
+function multiplyTwo() {
+  return x * 2;
+}
+function addThree() {
+  return x + 3;
+}
+
+function mathTest(result1, result2, result3) {
+  return result1() + result2() + result3();
+}
+
+console.log(mathTest(addOne, multiplyTwo, addThree)); */
+
+const story = "";
