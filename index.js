52e4099944873219fcf871448e92ce100c2db6f4
mr. whalen <michael.whalen@sitechhs.com>
2021-10-29 11:40:39 -0400

    fghdfgh

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const numbersArr = [1, 2, 3, 4, 5];
+/* const numbersArr = [1, 2, 3, 4, 5]; */
 
 /* numbersArr.forEach((number) => {
   //iterate through the array and run function on each element
@@ -7,9 +7,59 @@ const numbersArr = [1, 2, 3, 4, 5];
   console.log(numbersArr);
 }); */
 
-const newArr = numbersArr.map((number) => {
+/* const newArr = numbersArr.map((number) => {
   return number * 2;
 });
 const newArr = numbersArr.map((number) => number * 2);
 
 console.log(newArr);
+ */
+
+const characters = [
+  {
+    name: "Superman",
+    powers: ["Flight", "Strength", "Heat Vision"],
+    hero: true,
+  },
+  {
+    name: "Batman",
+    powers: ["Capitalism", "focused"],
+    hero: true,
+  },
+  {
+    name: "Spiderman",
+    powers: ["Spider-sense", "Strength", "smart"],
+    hero: true,
+  },
+  {
+    name: "Joker",
+    powers: ["crazy"],
+    hero: false,
+  },
+  {
+    name: "Lex Luthor",
+    powers: ["Capitalism", "smart"],
+    hero: false,
+  },
+  {
+    name: "Russell Koshkin",
+    powers: [
+      "Smells like a butt",
+      "disliked by his peers",
+      "bad at soccer",
+      "can't read the room",
+    ],
+    hero: false,
+  },
+  {
+    name: "Daniel",
+    powers: ["Loud", "Sussy Web Design", "Afraid of hair colors"],
+    hero: false,
+  },
+];
+
+characters
+  .filter((character) => character.hero === false)
+  .forEach((villainn) => {
+    console.log(villain.name);
+  });
