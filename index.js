47cda0c989f90fc7a0b604a911fe640ae1920e32
mr. whalen <michael.whalen@sitechhs.com>
2022-10-26 09:38:54 -0400

    f

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const background = document.querySelector(".background");
 const changeBtn = document.querySelector(".button");
 const headerTwo = document.querySelector(".header-two");
+
+function getRGB() {
+  let r = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
+  let g = Math.floor(Math.random() * 256);
+  return { r, g, b };
+}
+console.log(getRGB());
